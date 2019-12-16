440117cbd4b16b7402829f15bda3346929806311
Erik Huynh <Erik.W.Huynh@gmail.com>
2019-12-15 19:27:32 -0700

    Current weather generates properly

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-var cityStr = "";
-var main = $("#mainDisplay");
-
 function getCurrentWeather(location){
     $.ajax({
-    url: `http://api.openweathermap.org/data/2.5/weather?q=Salt+Lake+City&mode=JSON&units=imperial&APPID=9c5c1b3cee6e10229e2b0a0786d075e1`,
+    url: `http://api.openweathermap.org/data/2.5/weather?q= ${location} &mode=JSON&units=imperial&APPID=9c5c1b3cee6e10229e2b0a0786d075e1`,
     method: "GET"
   }).then(function(response) {
-      //Returns API data
-      //set variables for data gathered
-      var name = response.name;
-      var currentTemp = response.main.temp;
-      var description = response.weather[0].description;
-      var high = response.main.temp_max;
-      var low = response.main.temp_min;
-      var precip = response.main.humidity;
-      console.log(name);
-      console.log(description);
-      console.log(currentTemp);
-      console.log(high);
-      console.log(low);
-      console.log(precip);
-      //makes container for display
-      var div = $(`<div>Temperature: ${currentTemp}</div>`);
-      main.append(div);
-      //appends data to the display
+      //set API data to an object
+      var currentWeather = {
+        name: response.name,
+        currentTemp: response.main.temp,
+        description: response.weather[0].description,
+        high: response.main.temp_max,
+        low: response.main.temp_min,
+        precip: response.main.humidity,
+      }
+      new CurrentWeatherDisplay(currentWeather);
   });
-  }
+}
+
 $("#submitBtn").on("click", function(event){
     event.preventDefault();
     var city = $("#location").val();
-    cityStr = city.replace(/\ /g, '+');
+    var cityStr = city.replace(/\ /g, '+');
     getCurrentWeather(cityStr);
 });
+
+//Constructor function
+function CurrentWeatherDisplay(obj){
+    //Removes any existing .current
+    $(".current").remove();
+    //Creates div.current
+    var main = $(`<div class="current">`);
+    //Cycles through obj passed in as function
+    for (const key in obj) {
+        //Creates new <p> with obj key and value
+        var k= $(`<p>${[key]} ${obj[key]}</p>`);
+        main.append(k)
+    }
+    $("#mainDisplay").append(main);
+}
