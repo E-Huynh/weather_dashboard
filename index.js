848e223b36ae1b59f08498b89780f1f64f832c7e
Erik Huynh <Erik.W.Huynh@gmail.com>
2019-12-15 17:05:56 -0700

    Connected input to api

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-alert("buttstuff");
+var cityStr = "";
+
+function getWeather(location){
+    $.ajax({
+    url: `http://api.openweathermap.org/data/2.5/forecast?q=${location}&mode=JSON&APPID=9c5c1b3cee6e10229e2b0a0786d075e1`,
+    method: "GET"
+  }).then(function(response) {
+      console.log(response);
+      //Returns API data
+      //set variables for data gathered
+      //makes container for display
+      //appends data to the display
+  });
+  }
+$("#submitBtn").on("click", function(event){
+    event.preventDefault();
+    var city = $("#location").val();
+    cityStr = city.replace(/\ /g, '+');
+    getWeather(cityStr);
+});
