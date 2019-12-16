d3b138e15683cb44c9b527069a6e52a035e1cb95
Erik Huynh <Erik.W.Huynh@gmail.com>
2019-12-15 17:58:27 -0700

    get and set current variables from api

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 var cityStr = "";
+var main = $("#mainDisplay");
 
-function getWeather(location){
+function getCurrentWeather(location){
     $.ajax({
-    url: `http://api.openweathermap.org/data/2.5/forecast?q=${location}&mode=JSON&APPID=9c5c1b3cee6e10229e2b0a0786d075e1`,
+    url: `http://api.openweathermap.org/data/2.5/weather?q=Salt+Lake+City&mode=JSON&units=imperial&APPID=9c5c1b3cee6e10229e2b0a0786d075e1`,
     method: "GET"
   }).then(function(response) {
-      console.log(response);
       //Returns API data
       //set variables for data gathered
+      var name = response.name;
+      var currentTemp = response.main.temp;
+      var description = response.weather[0].description;
+      var high = response.main.temp_max;
+      var low = response.main.temp_min;
+      var precip = response.main.humidity;
+      console.log(name);
+      console.log(description);
+      console.log(currentTemp);
+      console.log(high);
+      console.log(low);
+      console.log(precip);
       //makes container for display
+      var div = $(`<div>Temperature: ${currentTemp}</div>`);
+      main.append(div);
       //appends data to the display
   });
   }
@@ -16,5 +30,5 @@ $("#submitBtn").on("click", function(event){
     event.preventDefault();
     var city = $("#location").val();
     cityStr = city.replace(/\ /g, '+');
-    getWeather(cityStr);
+    getCurrentWeather(cityStr);
 });
