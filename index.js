0c553c519ee4cf9687f82bff4e6c252a9c7abf83
Erik Huynh <Erik.W.Huynh@gmail.com>
2019-12-16 18:21:02 -0700

    Changed obj keys

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function getCurrentWeather(location){
     $.ajax({
-    url: `http://api.openweathermap.org/data/2.5/weather?q= ${location} &mode=JSON&units=imperial&APPID=9c5c1b3cee6e10229e2b0a0786d075e1`,
+        //Change this back to dynamic at the end
+    url: `http://api.openweathermap.org/data/2.5/weather?q=Salt+Lake+City&mode=JSON&units=imperial&APPID=9c5c1b3cee6e10229e2b0a0786d075e1`,
     method: "GET"
   }).then(function(response) {
       //set API data to an object
       var currentWeather = {
-        name: response.name,
-        currentTemp: response.main.temp,
-        description: response.weather[0].description,
-        high: response.main.temp_max,
-        low: response.main.temp_min,
-        precip: response.main.humidity,
+        City: response.name,
+        Temperature: response.main.temp,
+        Description: response.weather[0].description,
+        High: response.main.temp_max,
+        Low: response.main.temp_min,
+        Humidity: response.main.humidity,
       }
       new CurrentWeatherDisplay(currentWeather);
   });
@@ -32,7 +33,7 @@ function CurrentWeatherDisplay(obj){
     //Cycles through obj passed in as function
     for (const key in obj) {
         //Creates new <p> with obj key and value
-        var k= $(`<p>${[key]} ${obj[key]}</p>`);
+        var k= $(`<p>${[key]}: ${obj[key]}</p>`);
         main.append(k)
     }
     $("#mainDisplay").append(main);
