ce646ee84ee77ea9d5cf6bd2b001d18612dd0179
Erik Huynh <56613046+E-Huynh@users.noreply.github.com>
2020-01-05 18:56:58 -0700

    Changed http to https

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ $("#historyList").on("click", "button", function(event){
 
 function getCurrentWeather(location){
     $.ajax({
-    url: `http://api.openweathermap.org/data/2.5/weather?q=${location}&mode=JSON&units=imperial&APPID=9c5c1b3cee6e10229e2b0a0786d075e1`,
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${location}&mode=JSON&units=imperial&APPID=9c5c1b3cee6e10229e2b0a0786d075e1`,
     method: "GET"
   }).then(function(response) {
       //set API data to an object
@@ -84,7 +84,7 @@ function getCurrentWeather(location){
 
 function getFiveDayWeather(location){
     $.ajax({
-    url: `http://api.openweathermap.org/data/2.5/forecast?q=${location}&mode=JSON&units=imperial&appid=9c5c1b3cee6e10229e2b0a0786d075e1`,
+    url: `https://api.openweathermap.org/data/2.5/forecast?q=${location}&mode=JSON&units=imperial&appid=9c5c1b3cee6e10229e2b0a0786d075e1`,
     method: "GET"
   }).then(function(response) {
     //set API data to an object
